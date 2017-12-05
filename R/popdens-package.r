c54094ec2fa4fb12d4073dbdce294925d7443bd5
mpadge <mark.padgham@email.com>
2017-12-05 12:37:45 +0100

    add geotif processing functions

--- R/popdens-package.r
+++ R/popdens-package.r
@@ -2,4 +2,12 @@
 #'
 #' @name popdens
 #' @docType package
+#' @author Mark Padgham, Robin Lovelace
+#' @importFrom here here
+#' @importFrom magrittr %>% extract2
+#' @importFrom osmdata add_osm_feature getbb opq osmdata_sf trim_osmdata
+#' @importFrom osmdata osm_poly2line
+#' @importFrom raster crop raster projectRaster writeRaster
+#' @importFrom utils packageVersion setTxtProgressBar
+#' @importFrom utils txtProgressBar unzip
 NULL
